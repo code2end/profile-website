a539f46803150e6b766b40a36d48aa87109f304d
Aditya Goyal <adityagoyal619@gmail.com>
2024-07-04 19:45:51 +0530

    Fixed stars and forks issue from footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import styled from 'styled-components';
 import { Icon } from '@components/icons';
 import { socialMedia } from '@config';
@@ -67,68 +66,28 @@ const StyledCredit = styled.div`
   }
 `;
 
-const Footer = () => {
-  const [githubInfo, setGitHubInfo] = useState({
-    stars: null,
-    forks: null,
-  });
+const Footer = () => (
+  <StyledFooter>
+    <StyledSocialLinks>
+      <ul>
+        {socialMedia &&
+          socialMedia.map(({ name, url }, i) => (
+            <li key={i}>
+              <a href={url} aria-label={name}>
+                <Icon name={name} />
+              </a>
+            </li>
+          ))}
+      </ul>
+    </StyledSocialLinks>
 
-  useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      return;
-    }
-    fetch('https://api.github.com/repos/code2end/profile-website')
-      .then(response => response.json())
-      .then(json => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
-      .catch(e => console.error(e));
-  }, []);
-
-  return (
-    <StyledFooter>
-      <StyledSocialLinks>
-        <ul>
-          {socialMedia &&
-            socialMedia.map(({ name, url }, i) => (
-              <li key={i}>
-                <a href={url} aria-label={name}>
-                  <Icon name={name} />
-                </a>
-              </li>
-            ))}
-        </ul>
-      </StyledSocialLinks>
-
-      <StyledCredit tabindex="-1">
-        <div>
-          Designed by<a href="https://github.com/bchiang7/v4">Brittany Chiang</a>& edited by
-          <a href="https://github.com/code2end/profile-website">Aditya Goyal</a>
-        </div>
-
-        {githubInfo.stars && githubInfo.forks && (
-          <div className="github-stats">
-            <span>
-              <Icon name="Star" />
-              <span>{githubInfo.stars.toLocaleString()}</span>
-            </span>
-            <span>
-              <Icon name="Fork" />
-              <span>{githubInfo.forks.toLocaleString()}</span>
-            </span>
-          </div>
-        )}
-      </StyledCredit>
-    </StyledFooter>
-  );
-};
-
-Footer.propTypes = {
-  githubInfo: PropTypes.object,
-};
+    <StyledCredit tabindex="-1">
+      <div>
+        Designed by<a href="https://github.com/bchiang7/v4">Brittany Chiang</a>& edited by
+        <a href="https://github.com/code2end/profile-website">Aditya Goyal</a>
+      </div>
+    </StyledCredit>
+  </StyledFooter>
+);
 
 export default Footer;
